4681be4a12d6aa5fce50517aa91ee01d4834dee9
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-06-01 17:09:09 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,40 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+    description: `A simple site about pandas eating lots...`,
+    author: `Erich Keil via gatsbyjs`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: "src/utils/typography",
+      },
+    },
+  ],
 }
---
+++ src/pages/testing-that-md-order.md
@@ -0,0 +1,24 @@
+---
+title: testing that graphQl game yo!
+date: "2020-06-01"
+---
+
+# yeah my md skills aren't that strong
+
+## but I can get it with practice
+
+### i understand the concepts
+
+- just
+- not
+- familiar
+- with
+- all
+- the
+- syntax
+  - off
+  - the
+  - top
+  - of
+  - my
+  - head
